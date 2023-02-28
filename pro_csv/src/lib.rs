35c6f92ca2ed7fe620b842a9147f7154fba8506c
eVisualUser <sylvain.tosoni.dev@protonmail.com>
2023-02-28 19:57:53 +0100

    Added parsing csv &str

--- pro_csv/src/lib.rs
+++ pro_csv/src/lib.rs
@@ -1,14 +1,65 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[derive(Debug, Clone)]
+pub struct CSV {
+    separator: char,
+    buffer: Vec<Vec<String>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Default for CSV {
+    fn default() -> Self {
+        Self {
+            separator: ';',
+            buffer: Vec::new(),
+        }
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl CSV {
+    pub fn set_sperator_char(&mut self, separator: char) {
+        self.separator = separator;
+    }
+
+    pub fn get_sperator_char(&self) -> char {
+        self.separator
+    }
+}
+
+impl CSV {
+    pub fn load(&mut self, content: &str) {
+        let mut lines = Vec::<String>::new();
+        let mut line = String::new();
+        for i in content.chars() {
+            if i != '\n' {
+                line.push(i);
+            } else if !line.is_empty() {
+                lines.push(line);
+                line = String::new();
+            }
+        }
+        if !line.is_empty() {
+            lines.push(line);
+        }
+
+        for line in lines.iter() {
+            self.buffer.push(self.parse_line(&line));
+        }
+    }
+
+    pub fn parse_line(&self, line: &str) -> Vec<String> {
+        let mut result = Vec::<String>::new();
+
+        let mut buffer = String::new();
+        for i in line.chars() {
+            if i != self.separator {
+                buffer.push(i);
+            } else if !buffer.is_empty() {
+                result.push(buffer);
+                buffer = String::new();
+            }
+        }
+        if !buffer.is_empty() {
+            result.push(buffer);
+        }
+
+        result
     }
 }
